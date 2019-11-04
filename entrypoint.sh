f64f20e5e09133103d5736320970fcd5ea4a8b37
Antoine Monnet <amonnet@medici.tv>
2019-11-04 11:27:34 +0100

    add some log

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,24 @@ PERIOD=${PERIOD:-30}
 READY_FILE=${READY_FILE:-/.ready}
 
 function init() {
+	echo  "init"
 	touch $($RCLONE config file)
 	$RCLONE copy ${ARGS} ${REMOTE} /storage || return 1
-	touch /storage${READY_FILE}	
+	touch /storage${READY_FILE}
 }
 
 function sync() {
 	while true; do
 		sleep ${PERIOD}
+		echo "sync"
 		$RCLONE sync --exclude ${READY_FILE} ${ARGS} /storage ${REMOTE}
-	done	
+	done
 }
 
-case $0 in 
+case $0 in
 
 	/bin/check)
+		echo "check"
 		test -f /storage${READY_FILE}
 		;;
 

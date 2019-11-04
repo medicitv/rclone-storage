c9a78c1a0cc5c5bdfaaeb377a1738a7a04e5626d
Antoine Monnet <amonnet@medici.tv>
2019-11-04 12:08:53 +0100

    handle UID and GID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
-RCLONE=rclone
 PERIOD=${PERIOD:-30}
 READY_FILE=${READY_FILE:-/.ready}
+CONFIG_FILE="--config /tmp/rclone.conf"
+
+if [ -n "$UID" ]; then
+	GID=${GID:-${UID}}
+	group=$(getent group $GID | cut -d ':' -f 1)
+	if [ -z "$group" ]; then
+		addgroup -g ${GID} user
+		group="user"
+	fi
+	user=$(getent passwd $UID | cut -d ':' -f 1)
+	if [ -z "$user" ]; then
+		adduser -u ${UID} -G "${group}" -D user
+		user="user"
+	fi
+	chown -R $user:$group /storage
+	SU="su $user -c"
+else
+	SU="eval"
+fi
 
 function init() {
 	echo  "init"
-	touch $($RCLONE config file)
-	$RCLONE copy ${ARGS} ${REMOTE} /storage || return 1
-	touch /storage${READY_FILE}
+
+	$SU "touch /tmp/rclone.conf"
+	$SU "rclone copy ${CONFIG_FILE} ${ARGS} ${REMOTE} /storage" || return 1
+	$SU "touch /storage${READY_FILE}"
 }
 
 function sync() {
 	while true; do
 		sleep ${PERIOD}
 		echo "sync"
-		$RCLONE sync --exclude ${READY_FILE} ${ARGS} /storage ${REMOTE}
+		$SU "rclone sync ${CONFIG_FILE} --exclude ${READY_FILE} ${ARGS} /storage ${REMOTE}"
 	done
 }
 
